444a009cf03d31eedac446e8e8b6066a5ec7c36d
Rodrigo Longo <rodri@MacBook-Pro-de-Rodrigo.local>
2025-01-24 11:00:30 +0100

    feature/incluir precio base

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import { useState } from "react";
 export default function Home() {
   const [cantidad, setCantidad] = useState(0);
   const [percentage, setPercentage] = useState(21);
+  const [baseTotal, setBaseTotal] = useState(0);
   const [ivaTotal, setIvaTotal] = useState(0);
   const [resultado, setResultado] = useState(0);
   const [modoCalculo, setModoCalculo] = useState("extra"); // 'extra' o 'incluido'
@@ -12,18 +13,21 @@ export default function Home() {
     const cantidadNum = parseFloat(c) || 0;
     const porcentajeNum = parseFloat(p) || 0;
 
-    let iva, total;
+    let iva, total, base;
 
     if (modo === "extra") {
       // IVA añadido al valor base
+      base = cantidadNum;
       iva = cantidadNum * (porcentajeNum / 100);
       total = cantidadNum + iva;
     } else if (modo === "incluido") {
       // IVA ya incluido en el valor base
       iva = cantidadNum - cantidadNum / (1 + porcentajeNum / 100);
       total = cantidadNum;
+      base = total - iva;
     }
 
+    setBaseTotal(base.toFixed(2)); // Mostrar con 2 decimales
     setIvaTotal(iva.toFixed(2)); // Mostrar con 2 decimales
     setResultado(total.toFixed(2)); // Mostrar con 2 decimales
   };
@@ -91,6 +95,9 @@ export default function Home() {
           </div>
 
           <div className='page_results'>
+            <div className='result'>
+              <p>Base:</p> <p className='span'>{baseTotal} €</p>
+            </div>
             <div className='result'>
               <p>IVA:</p> <p className='span'>{ivaTotal} €</p>
             </div>
